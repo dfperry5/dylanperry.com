80164fc2eb3149856792f596b84c877b16c2c11f
dfperry5 <DylanPerry5@gmail.com>
2017-06-03 22:18:58 -0400

    Creating smaller components

--- src/components/screens/About/index.js
+++ src/components/screens/About/index.js
@@ -1,18 +1,73 @@
 // src/components/About/index.js
 import React, { PropTypes, Component } from 'react';
 import classnames from 'classnames';
-
 import NCState from './NCState.jpg'
 import Finley from './finley.jpg';
 import Family from './family.JPG';
 import Ariel from './DylanRelWedding.jpg'
 import DylanHS from './DylanHeadshot.jpg'
 import './style.css';
+// Components
+import AboutMeRow from '../../shared/about-me-row'
+import AboutMeRowReverse from '../../shared/about-me-row-reverse'
 
 export default class About extends Component {
   // static propTypes = {}
   // static defaultProps = {}
   // state = {}
+  constructor() {
+    super()
+    this.state = {
+      family: {
+        quote: {
+          text: '"But as for me and my house, we will serve the Lord."',
+          author: 'Joshua 24:15',
+          source: 'The Bible (ESV)'
+        },
+        title: "Family",
+        post: {
+          lead: "June 19 1993 - the day my parents' lives got immeasurably better.",
+          text: "I'm from the small town of Bunn, North Carolina. I was blessed with the most supporting family anyone could ask for. Without them, I wouldn't be half the person I am today. They taught me how to love, how to follow God, and how to be a man. I really do owe everything I have and everything I am to the lessons they taught me."
+        }
+      },
+      ncState: {
+        quote:{
+          text: '"For the strength of the pack is the wolf, and the strength of the wolf is the pack.”',
+          author: 'Olaf the Snowman',
+          source: 'Law of the Jungle',
+        },
+        title: "NC State",
+        post: {
+          lead: "I graduated Magna Cum Laude from NC State in May 2015, with a B.S. in Computer Science.",
+          text: "A little more than 4 years before then, I fell in love with everything about the campus and the school. While there, I joined the fraternity Delta Kappa Epsilon where I made some life long friends. One of my favorite things in life is getting together with friends to watch Wolfpack football."
+        }
+      },
+      ariel: {
+        quote:{
+          text: '"Some people are worth melting for.”',
+          author: 'Rudyard Kipling',
+          source: 'Frozen',
+        },
+        title: "Ariel",
+        post: {
+          lead: "September 19th, 2015 is where this story began.",
+          text: " It really began a little before that -- but I'll leave that part to your imagination. I am so lucky to have the most supporting, wonderful, beautiful, and insane girlfriend in the world."
+        }
+      },
+      finley: {
+         quote:{
+          text: '"Happiness is a warm puppy."',
+          author: 'Charles Shultz',
+          source: 'Peanuts Cartoons',
+        },
+        title: "Finley",
+        post: {
+          lead: "On July 4th 2016, my life improved immensely.",
+          text: "We adopted our puppy - Finley Forest. He has brought me so much joy, and taught me so much about what responsibility really is. He's and essential and crucial part of our family. Obviously, I'm a dog person."
+        }
+      }
+    }
+  }
 
   render() {
     const { className, ...props } = this.props;
@@ -20,93 +75,20 @@ export default class About extends Component {
       <div className={classnames('About', className)} {...props}>
 
         <div className="container">
-            <div className="container-fluid">
-              <h1> About <img className="headshot" src={DylanHS} /> Me: </h1>
-            </div>
-            <br /> <br />
-            <div className="row">
-              <div className="col-4">
-                <h1> Family </h1>
-                <img className="img-fluid" src={Family}/>
-                </div>
-              <div className="col-5">
-                <blockquote className="blockquote blockquote-reverse">
-                  <p className="mb-0">"But as for me and my house, we will serve the Lord.”</p>
-                  <footer className="blockquote-footer">Joshua 24:15 <cite title="Source Title">The Bible (ESV)</cite></footer>
-                </blockquote>
-                <p className="lead">
-                  June 19 1993 - the day my parents lives got immeasurebly better.
-                </p>
-                <p>
-                  I'm from the small town of Bunn, North Carolina. I was blessed with the most supporting family anyone could ask for. Without them, I wouldn't be half the person I am today. They taught me how to love, how to follow God, and how to be a man. I really do owe everything I have and everything I am to the lessons they taught me.
-                </p>
-              </div>
-            </div>
-
-          <hr />
-
-          <div className="row">
-            <div className="col-5">
-              <blockquote className="blockquote blockquote-reverse">
-                <p className="mb-0">For the strength of the pack is the wolf, and the strength of the wolf is the pack.</p>
-                <footer className="blockquote-footer">Rudyard Kipling <cite title="Source Title">Law of the Jungle</cite></footer>
-              </blockquote>
-              <p className="lead">
-                I graduated Magna Cum Laude from NC State in May 2015, with a B.S. in Computer Science.
-              </p>
-              <p>
-                A little more than 4 years before then, I fell in love with everything about the campus and the school. While there, I joined the fraternity Delta Kappa Epsilon where I made some life long friends. One of my favorite things in life is getting together with friends to watch Wolfpack football.  
-              </p>
-            </div>
-            <div className="col-4">
-              <h1> NC State </h1>
-              <img className="img-fluid" src={NCState}/>
-            </div>
+          <div className="container-fluid">
+            <h1> About <img className="headshot" src={DylanHS} /> Me: </h1>
           </div>
-
+          <br /> <br />
+            
+          <AboutMeRow title={this.state.family.title} img={Family} quote={this.state.family.quote} post={this.state.family.post}/>
+          <hr />
+          <AboutMeRowReverse title={this.state.ncState.title} img={NCState} quote={this.state.ncState.quote} post={this.state.ncState.post}/>
           <hr /> 
-
-          <div className="row">
-              <div className="col-4">
-                <h1> Ariel </h1>
-                <img className="img-fluid" src={Ariel}/>
-                </div>
-              <div className="col-5">
-                <blockquote className="blockquote blockquote-reverse">
-                  <p className="mb-0">"Some people are worth melting for.”</p>
-                  <footer className="blockquote-footer">Olaf the Snowman <cite title="Source Title">Frozen</cite></footer>
-                </blockquote>
-                <p className="lead">
-                  September 19th, 2015 is where this story began.
-                </p>
-                <p>
-                  It really began a little before that -- but I'll leave that part to your imagination. I am so lucky to have the most supporting, wonderful, beautiful, and most insane girlfriend in the world.
-                </p>
-              </div>
-            </div>
-
+          <AboutMeRow title={this.state.ariel.title} img={Ariel} quote={this.state.ariel.quote} post={this.state.ariel.post}/>
           <hr />
-          
-          <div className="row">
-            <div className="col-5">
-              <blockquote className="blockquote blockquote-reverse">
-                <p className="mb-0">"Happiness is a warm puppy.”</p>
-                <footer className="blockquote-footer">Charles Shultz <cite title="Source Title">Peanuts Cartoons</cite></footer>
-              </blockquote>
-              <p className="lead">
-                On July 4th 2016, my life improved immensely.
-              </p>
-              <p>
-                We adopted our puppy - Finley Forest Tipton-Perry. He has brought me so much joy, and taught me so much about what responsibility really is. He's and essential and crucial part of our family. Obviously, I'm a dog person.
-              </p>
-            </div>
-            <div className="col-4">
-              <h1> Finley </h1>
-              <img className="img-fluid" src={Finley}/>
-            </div>
-          </div>
-        </div>
+          <AboutMeRowReverse title={this.state.finley.title} img={Finley} quote={this.state.finley.quote} post={this.state.finley.post}/>
 
+        </div>
       </div>
     );
   }
---
+++ src/components/shared/about-me-row/index.js
@@ -0,0 +1,34 @@
+// src/components/About/index.js
+import React, { PropTypes, Component } from 'react';
+import classnames from 'classnames';
+import './style.css';
+
+export default class AboutMeRow extends Component {
+
+  render() {
+    return (
+      <div>
+        <div className="row">
+            <div className="col-4">
+            <h1> {this.props.title} </h1>
+            <img className="img-fluid" src={this.props.img}/>
+            </div>
+            <div className="col-5">
+            <blockquote className="blockquote blockquote-reverse">
+                <p className="mb-0">{this.props.quote.text}</p>
+                <footer className="blockquote-footer">{this.props.quote.author} <cite title="Source Title">{this.props.quote.source}</cite></footer>
+            </blockquote>
+            <p className="lead">
+                {this.props.post.lead}
+            </p>
+            <p>
+                {this.props.post.text}
+            </p>
+            </div>
+        </div>
+    </div>
+
+
+    );
+  }
+}
