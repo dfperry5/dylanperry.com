90dba8b9936ff39ca690a3293ae226aa239674aa
dfperry5 <DylanPerry5@gmail.com>
2017-06-13 22:08:13 -0400

    empty components

---
+++ src/components/shared/score-card/index.js
@@ -0,0 +1,14 @@
+// src/components/sports-scores/index.js
+import React, { PropTypes, Component } from 'react';
+import classnames from 'classnames';
+import './style.css';
+
+export default class ScoreCard extends Component {
+  render() {
+    return (
+      <div>
+          <h1> My Team card </h1>
+     </div>
+    );
+  }
+}
